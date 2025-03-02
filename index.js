45e397903dae31a49d22dbf0c57bf1db0a542cc8
Dinesh107 <dineshjr107@gmail.com>
2025-03-02 15:49:04 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,121 @@
+// printf 
+// cout 
+// systemoutprintln()
+// print
+// let studentName = "Varshini";
+// console.log("Welcome to js students!!!");
+// console.log("i like briyani");
+
+// window.alert("Welcome to js students")
+
+
+// document.getElementById("myH1").textContent = "Hello varshini!!!"
+
+// document.getElementById("myP").innerHTML = "Hello vaishnavi"
+
+// document.getElementById("myP2").innerText = "This is Dinesh"
+
+
+// variable = it is container that stores a value
+
+// behaves as if were the value it contains
+
+
+// declaration let x 
+// assignment x = 100;
+
+// let var const 
+
+// let x = 100;
+// console.log(x);
+
+
+// let age = 12;
+// let price = 99;
+// let gpa = 9.11;
+// let online = true;
+// let name = "varshini";
+
+
+// console.log(typeof age);
+// console.log(typeof online);
+// console.log(typeof name);
+
+
+// console.log(`You are ${age} years old, the price is ${price} and your cgpa is ${gpa}`);
+
+
+// let fullName = "Varshini";
+// let age = 25;
+// let student = false;
+
+// document.getElementById("p1").textContent = fullName;
+// document.getElementById("p2").textContent = age;
+// document.getElementById("p3").textContent = student;
+
+// Arithemtic operators - operands (values, variables, etc)
+                        // operator ( +, -, *, /, %, ** )
+                        
+// let student = 10;
+
+// student = student + 1;
+// student = student - 1;
+// student = student * 2;
+// student = student / 2;
+// student = student % 2;
+
+// student += 1;
+// student -= 1;
+// student *= 2;
+// student /= 2;
+// student %= 2;
+
+
+
+// console.log(student);
+
+// how to accept user input 
+
+//easyway - window promt
+// professional way
+
+// let username = window.prompt("Enter your username");
+
+// console.log(username);
+
+
+let username;
+
+document.getElementById("myBtn").onclick = function() {
+   username = document.getElementById("myText").value;
+   console.log(username);
+   document.getElementById("myH1").textContent = `Hello ${username}, Its Great to study!!!`;
+}
+
+
+// username
+// email  
+// password
+// phone
+// dateofbirth
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
